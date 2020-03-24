c934a0d32b11edc4d074191d049a16c42fcfbe49
ddxy18 <1206294583@qq.com>
2020-03-24 15:36:19 +0800

    add basic get and remove function

--- kernel/lib/list.h
+++ kernel/lib/list.h
@@ -23,4 +23,32 @@ void insert_to_head(linked_list_t list, linked_list_node_t *new_node) {
     tmp->next = new_node;
 }
 
+linked_list_node_t *get(linked_list_t list, pointer_t data) {
+    linked_list_node_t *tmp = list.head;
+    while (tmp->next != NULL) {
+        if (tmp->next->data == data) {
+            return tmp->next;
+        }
+    }
+    return NULL;
+}
+
+/**
+ * Remove 'remove_node' from 'list' and set 'remove_node.next' to 'NULL'. If 'remove_node' doesn't exist, return 'NULL'.
+ * @param list
+ * @param remove_node
+ * @return Return 'remove_node' whose field 'next' is set to 'NULL'. If 'remove_node' doesn't exist, return 'NULL'.
+ */
+linked_list_node_t *remove(linked_list_t list, linked_list_node_t *remove_node) {
+    linked_list_node_t *tmp = list.head;
+    while (tmp->next != NULL) {
+        if (tmp->next == remove_node) {
+            tmp->next = remove_node->next;
+            remove_node->next = NULL;
+            return remove_node;
+        }
+    }
+    return NULL;
+}
+
 #endif //XYOS_LIST_H
