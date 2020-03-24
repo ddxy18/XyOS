642960575e04cce2e5e177b9ed8efbdfc48c49fa
ddxy18 <1206294583@qq.com>
2020-03-24 10:51:43 +0800

    complete segments

---
+++ kernel/boot/hardware.h
@@ -0,0 +1,11 @@
+//
+// Created by dxy on 2020/3/20.
+//
+
+#ifndef XYOS_HARDWARE_H
+#define XYOS_HARDWARE_H
+
+// all represented in KB
+#define MIN_LOWER_MEMORY 600u
+#define MIN_UPPER_MEMORY 65536u
+#endif //XYOS_HARDWARE_H
