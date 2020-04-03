3098ba83579ebb6cf881d21d3609c6aba522c08f
ddxy18 <1206294583@qq.com>
2020-04-03 20:12:23 +0800

    add conditional compile

--- kernel/boot/multiboot.h
+++ kernel/boot/multiboot.h
@@ -29,6 +29,8 @@
 #define MULTIBOOT_HEADER_MAGIC 0x1BADB002
 // This should be in %eax.
 #define MULTIBOOT_BOOTLOADER_MAGIC 0x2BADB002
+// flags
+#define MULTIBOOT_HEADER_FLAGS MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
 // Alignment of multiboot modules.
 #define MULTIBOOT_MOD_ALIGN 0x00001000
 // Alignment of the multiboot info structure.
@@ -70,8 +72,8 @@
 #define MULTIBOOT_INFO_VBE_INFO 0x00000800
 #define MULTIBOOT_INFO_FRAMEBUFFER_INFO 0x00001000
 
-#ifndef BOOT
-#define BOOT
+#ifndef XYOS_BOOT_H
+#define XYOS_BOOT_H
 
 typedef unsigned char multiboot_uint8_t;
 typedef unsigned short multiboot_uint16_t;
@@ -79,141 +81,141 @@ typedef unsigned int multiboot_uint32_t;
 typedef unsigned long long multiboot_uint64_t;
 
 struct multiboot_header {
-  // Must be MULTIBOOT MAGIC - see above.
-  multiboot_uint32_t magic;
-  // Feature flags.
-  multiboot_uint32_t flags;
-  // The above fields plus this one must equal 0 mod 2^32.
-  multiboot_uint32_t checksum;
-  // These are only valid if MULTIBOOT AOUT KLUDGE is set.
-  multiboot_uint32_t header_addr;
-  multiboot_uint32_t load_addr;
-  multiboot_uint32_t load_end_addr;
-  multiboot_uint32_t bss_end_addr;
-  multiboot_uint32_t entry_addr;
-  // These are only valid if MULTIBOOT VIDEO MODE is set.
-  multiboot_uint32_t mode_type;
-  multiboot_uint32_t width;
-  multiboot_uint32_t height;
-  multiboot_uint32_t depth;
+    // Must be MULTIBOOT MAGIC - see above.
+    multiboot_uint32_t magic;
+    // Feature flags.
+    multiboot_uint32_t flags;
+    // The above fields plus this one must equal 0 mod 2^32.
+    multiboot_uint32_t checksum;
+    // These are only valid if MULTIBOOT AOUT KLUDGE is set.
+    multiboot_uint32_t header_addr;
+    multiboot_uint32_t load_addr;
+    multiboot_uint32_t load_end_addr;
+    multiboot_uint32_t bss_end_addr;
+    multiboot_uint32_t entry_addr;
+    // These are only valid if MULTIBOOT VIDEO MODE is set.
+    multiboot_uint32_t mode_type;
+    multiboot_uint32_t width;
+    multiboot_uint32_t height;
+    multiboot_uint32_t depth;
 };
 
 // The symbol table for a.out.
 typedef struct multiboot_aout_symbol_table {
-  multiboot_uint32_t tabsize;
-  multiboot_uint32_t strsize;
-  multiboot_uint32_t addr;
-  multiboot_uint32_t reserved;
+    multiboot_uint32_t tabsize;
+    multiboot_uint32_t strsize;
+    multiboot_uint32_t addr;
+    multiboot_uint32_t reserved;
 } multiboot_aout_symbol_table_t;
 
 // The section header table for ELF.
 typedef struct multiboot_elf_section_header_table {
-  multiboot_uint32_t num;
-  multiboot_uint32_t size;
-  multiboot_uint32_t addr;
-  multiboot_uint32_t shndx;
+    multiboot_uint32_t num;
+    multiboot_uint32_t size;
+    multiboot_uint32_t addr;
+    multiboot_uint32_t shndx;
 } multiboot_elf_section_header_table_t;
 
 typedef struct multiboot_info {
-  // Multiboot info version number
-  multiboot_uint32_t flags;
-  // Available memory from BIOS
-  multiboot_uint32_t mem_lower;
-  multiboot_uint32_t mem_upper;
-  // "root" partition
-  multiboot_uint32_t boot_device;
-  // Kernel command line
-  multiboot_uint32_t cmdline;
-  // Boot-Module list
-  multiboot_uint32_t mods_count;
-  multiboot_uint32_t mods_addr;
-  union {
-    multiboot_aout_symbol_table_t aout_sym;
-    multiboot_elf_section_header_table_t elf_sec;
-  } u;
-  // Memory Mapping buffer
-  multiboot_uint32_t mmap_length;
-  multiboot_uint32_t mmap_addr;
-  // Drive Info buffer
-  multiboot_uint32_t drives_length;
-  multiboot_uint32_t drives_addr;
-  // ROM configuration table
-  multiboot_uint32_t config_table;
-  // Boot Loader Name
-  multiboot_uint32_t boot_loader_name;
-  // APM table
-  multiboot_uint32_t apm_table;
-  // Video
-  multiboot_uint32_t vbe_control_info;
-  multiboot_uint32_t vbe_mode_info;
-  multiboot_uint16_t vbe_mode;
-  multiboot_uint16_t vbe_interface_seg;
-  multiboot_uint16_t vbe_interface_off;
-  multiboot_uint16_t vbe_interface_len;
-  multiboot_uint64_t framebuffer_addr;
-  multiboot_uint32_t framebuffer_pitch;
-  multiboot_uint32_t framebuffer_width;
-  multiboot_uint32_t framebuffer_height;
-  multiboot_uint8_t framebuffer_bpp;
+    // Multiboot info version number
+    multiboot_uint32_t flags;
+    // Available memory from BIOS
+    multiboot_uint32_t mem_lower;
+    multiboot_uint32_t mem_upper;
+    // "root" partition
+    multiboot_uint32_t boot_device;
+    // Kernel command line
+    multiboot_uint32_t cmdline;
+    // Boot-Module list
+    multiboot_uint32_t mods_count;
+    multiboot_uint32_t mods_addr;
+    union {
+        multiboot_aout_symbol_table_t aout_sym;
+        multiboot_elf_section_header_table_t elf_sec;
+    } u;
+    // Memory Mapping buffer
+    multiboot_uint32_t mmap_length;
+    multiboot_uint32_t mmap_addr;
+    // Drive Info buffer
+    multiboot_uint32_t drives_length;
+    multiboot_uint32_t drives_addr;
+    // ROM configuration table
+    multiboot_uint32_t config_table;
+    // Boot Loader Name
+    multiboot_uint32_t boot_loader_name;
+    // APM table
+    multiboot_uint32_t apm_table;
+    // Video
+    multiboot_uint32_t vbe_control_info;
+    multiboot_uint32_t vbe_mode_info;
+    multiboot_uint16_t vbe_mode;
+    multiboot_uint16_t vbe_interface_seg;
+    multiboot_uint16_t vbe_interface_off;
+    multiboot_uint16_t vbe_interface_len;
+    multiboot_uint64_t framebuffer_addr;
+    multiboot_uint32_t framebuffer_pitch;
+    multiboot_uint32_t framebuffer_width;
+    multiboot_uint32_t framebuffer_height;
+    multiboot_uint8_t framebuffer_bpp;
 #define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED 0
 #define MULTIBOOT_FRAMEBUFFER_TYPE_RGB 1
 #define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT 2
-  multiboot_uint8_t framebuffer_type;
-  union {
-    struct {
-      multiboot_uint32_t framebuffer_palette_addr;
-      multiboot_uint16_t framebuffer_palette_num_colors;
+    multiboot_uint8_t framebuffer_type;
+    union {
+        struct {
+            multiboot_uint32_t framebuffer_palette_addr;
+            multiboot_uint16_t framebuffer_palette_num_colors;
+        };
+        struct {
+            multiboot_uint8_t framebuffer_red_field_position;
+            multiboot_uint8_t framebuffer_red_mask_size;
+            multiboot_uint8_t framebuffer_green_field_position;
+            multiboot_uint8_t framebuffer_green_mask_size;
+            multiboot_uint8_t framebuffer_blue_field_position;
+            multiboot_uint8_t framebuffer_blue_mask_size;
+        };
     };
-    struct {
-      multiboot_uint8_t framebuffer_red_field_position;
-      multiboot_uint8_t framebuffer_red_mask_size;
-      multiboot_uint8_t framebuffer_green_field_position;
-      multiboot_uint8_t framebuffer_green_mask_size;
-      multiboot_uint8_t framebuffer_blue_field_position;
-      multiboot_uint8_t framebuffer_blue_mask_size;
-    };
-  };
 } multiboot_info_t;
 
 struct multiboot_color {
-  multiboot_uint8_t red;
-  multiboot_uint8_t green;
-  multiboot_uint8_t blue;
+    multiboot_uint8_t red;
+    multiboot_uint8_t green;
+    multiboot_uint8_t blue;
 };
 
 typedef struct multiboot_mmap_entry {
-  multiboot_uint32_t size;
-  multiboot_uint64_t addr;
-  multiboot_uint64_t len;
+    multiboot_uint32_t size;
+    multiboot_uint64_t addr;
+    multiboot_uint64_t len;
 #define MULTIBOOT_MEMORY_AVAILABLE 1
 #define MULTIBOOT_MEMORY_RESERVED 2
 #define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE 3
 #define MULTIBOOT_MEMORY_NVS 4
 #define MULTIBOOT_MEMORY_BADRAM 5
-  multiboot_uint32_t type;
+    multiboot_uint32_t type;
 } __attribute__((packed)) multiboot_memory_map_t;
 
 typedef struct multiboot_mod_list {
-  // the memory used goes from bytes ’mod start’ to ’mod end-1’ inclusive
-  multiboot_uint32_t mod_start;
-  multiboot_uint32_t mod_end;
-  // Module command line
-  multiboot_uint32_t cmdline;
-  // padding to take it to 16 bytes (must be zero)
-  multiboot_uint32_t pad;
+    // the memory used goes from bytes ’mod start’ to ’mod end-1’ inclusive
+    multiboot_uint32_t mod_start;
+    multiboot_uint32_t mod_end;
+    // Module command line
+    multiboot_uint32_t cmdline;
+    // padding to take it to 16 bytes (must be zero)
+    multiboot_uint32_t pad;
 } multiboot_module_t;
 
 // APM BIOS info.
 struct multiboot_apm_info {
-  multiboot_uint16_t version;
-  multiboot_uint16_t cseg;
-  multiboot_uint32_t offset;
-  multiboot_uint16_t cseg_16;
-  multiboot_uint16_t dseg;
-  multiboot_uint16_t flags;
-  multiboot_uint16_t cseg_len;
-  multiboot_uint16_t cseg_16_len;
-  multiboot_uint16_t dseg_len;
+    multiboot_uint16_t version;
+    multiboot_uint16_t cseg;
+    multiboot_uint32_t offset;
+    multiboot_uint16_t cseg_16;
+    multiboot_uint16_t dseg;
+    multiboot_uint16_t flags;
+    multiboot_uint16_t cseg_len;
+    multiboot_uint16_t cseg_16_len;
+    multiboot_uint16_t dseg_len;
 };
-#endif // ! BOOT
-#endif // ! MULTIBOOT HEADER
+#endif // XYOS_BOOT_H
+#endif // MULTIBOOT HEADER
--- kernel/memory/segment.h
+++ kernel/memory/segment.h
@@ -2,10 +2,8 @@
 // Created by dxy on 2020/3/16.
 //
 
-#ifndef XYOS_SEG_H
-#define XYOS_SEG_H
-
-#include <stdint.h>
+#ifndef XYOS_SEGMENT_H
+#define XYOS_SEGMENT_H
 
 /**
  * virtual address of GDT
@@ -47,35 +45,42 @@
 
 #define access(priority, type) (priority << 4u) | type
 
-typedef struct seg_descriptor {
-  // low 32 bits
-  uint16_t limit_0_15;
-  uint16_t base_addr_0_15;
+#ifndef XYOS_INTR_ENTRY_H
+#define XYOS_INTR_ENTRY_H
 
-  // high 32 bits
-  uint8_t base_addr_16_23;
-  // 4 bits type field
-  // descriptor type flag
-  // 2 bits descriptor privilege level field
-  // SEG-present flag
-  uint8_t access;
-  uint8_t limit_16_19 : 4;
-  // available and reserved bits
-  // L (64-bit code SEG) flag
-  // D/B (default operation size/default stack pointer size and/or upper bound)
-  // flag granularity flag
-  uint8_t flags : 4;
-  uint8_t base_addr_24_31;
+#include <stdint.h>
+
+typedef struct seg_descriptor {
+    // low 32 bits
+    uint16_t limit_0_15;
+    uint16_t base_addr_0_15;
+
+    // high 32 bits
+    uint8_t base_addr_16_23;
+    // 4 bits type field
+    // descriptor type flag
+    // 2 bits descriptor privilege level field
+    // SEG-present flag
+    uint8_t access;
+    uint8_t limit_16_19: 4;
+    // available and reserved bits
+    // L (64-bit code SEG) flag
+    // D/B (default operation size/default stack pointer size and/or upper bound)
+    // flag granularity flag
+    uint8_t flags: 4;
+    uint8_t base_addr_24_31;
 } __attribute__((packed)) seg_descriptor_t;
 
 /**
  * structure of register GDTR, which should contain information of GDT
  */
 typedef struct GDTR {
-  uint16_t limit;
-  uint32_t base_addr;
+    uint16_t limit;
+    uint32_t base_addr;
 } __attribute__((packed)) GDTR_t;
 
 void gdt_init();
 
-#endif // XYOS_SEG_H
+#endif // XYOS_INTR_ENTRY_H
+
+#endif // XYOS_SEGMENT_H
