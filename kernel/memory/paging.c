95ea0248237148dda456b8c7cd393e0b70df6fd5
ddxy18 <1206294583@qq.com>
2020-04-05 23:52:35 +0800

    add page fault handler

--- kernel/memory/paging.c
+++ kernel/memory/paging.c
@@ -11,104 +11,155 @@
 
 #include "paging.h"
 #include "page_allocator.h"
+#include "../lib/asm_wrapper.h"
+#include "../lib/bit.h"
+#include "direct_mapping_allocator.h"
+#include "../process/process.h"
 
-CR3_t cr3;
+CR3_t k_cr3;
 
-void set_page_table_entry(page_table_entry_t *, uint8_t, uint32_t);
+void set_page_table_entry(pte_t *, uint8_t, uint32_t);
 
 void k_paging_init() {
-  cr3.page_dir_addr = pde_addr(K_PAGE_DIR_PHYSICAL_ADDR);
-  cr3.access = 0;
-  cr3.ignored = 0;
+    k_cr3.page_dir_addr = pd_addr(direct_mapping_phys_addr(K_PD_ADDR));
+    k_cr3.access = 0;
+    k_cr3.ignored = 0;
 
-  page_dir_entry_t *k_page_dir = (page_dir_entry_t *)(cr3.page_dir_addr << 12u);
+    pde_t *k_page_dir = (pde_t *) direct_mapping_phys_addr(K_PD_ADDR);
 
-  uintptr_t pt_addr = K_PAGE_TABLE_ADDR;
-  // Map first 4 MB memory to the same physical address. So when opening paging,
-  // current address can also be used.
-  k_page_dir[0].access = K_PDE_WRITE_PRESENT_ACCESS;
-  k_page_dir[0].ignored = 0x0u;
-  k_page_dir[0].page_table_addr = pde_addr(pt_addr);
-  page_table_entry_t *page_table = (page_dir_entry_t *)pt_addr;
-  for (uint32_t j = 0; j < PTE_MAX_NUM; ++j) {
-    page_table[j].access = K_PTE_WRITE_PRESENT_ACCESS;
-    page_table[j].ignored = 0x0u;
-    page_table[j].page_addr = pte_addr(j * PAGE_SIZE);
-  }
-  pt_addr += PAGE_TABLE_MAX_SIZE;
-  // set remaining user space's page dir entries to absent state
-  uint32_t i = 1;
-  for (; i < U_PDE_NUM; ++i) {
-    k_page_dir[i].access = U_PDE_WRITE_ABSENT_ACCESS;
-    k_page_dir[i].ignored = 0x0u;
-    k_page_dir[i].page_table_addr = PDE_ABSENT_ADDR;
-  }
-  // initialize direct mapping area
-    uintptr_t page_addr = 0x0u;
-  for (; i < K_PDE_DIRECT_MAP_NUM + U_PDE_NUM; ++i) {
-    k_page_dir[i].access = K_PDE_WRITE_PRESENT_ACCESS;
-    k_page_dir[i].ignored = 0x0u;
-    k_page_dir[i].page_table_addr = pde_addr(pt_addr);
-
-    page_table_entry_t *page_table = (page_table_entry_t *)pt_addr;
+    uintptr_t pt_addr = direct_mapping_phys_addr(K_PT_ADDR);
+    // Map first 4 MB memory to the same physical address. So when opening paging,
+    // current address can also be used.
+    k_page_dir[0].access = K_PDE_WRITE_PRESENT_ACCESS;
+    k_page_dir[0].ignored = 0x0u;
+    k_page_dir[0].page_table_addr = pt_addr(pt_addr);
+    pte_t *page_table = (pte_t *) pt_addr;
     for (uint32_t j = 0; j < PTE_MAX_NUM; ++j) {
-      page_table[j].access = K_PTE_WRITE_PRESENT_ACCESS;
-      page_table[j].ignored = 0x0u;
-      page_table[j].page_addr = pte_addr(page_addr);
-      page_addr += PAGE_SIZE;
+        page_table[j].access = K_PTE_WRITE_PRESENT_ACCESS;
+        page_table[j].ignored = 0x0u;
+        page_table[j].page_addr = page_addr(j * PAGE_SIZE);
     }
-    pt_addr += PAGE_TABLE_MAX_SIZE;
-  }
-  // set all dynamic mapping area's page dir entries to absent state
-  for (; i < PDE_NUM; ++i) {
-    k_page_dir[i].access = K_PDE_WRITE_ABSENT_ACCESS;
-    k_page_dir[i].ignored = 0x0u;
-    k_page_dir[i].page_table_addr = PDE_ABSENT_ADDR;
-  }
+    pt_addr += PT_MAX_SIZE;
+    // set remaining user space's page dir entries to absent state
+    uint32_t i = 1;
+    for (; i < U_PDE_NUM; ++i) {
+        k_page_dir[i].access = U_PDE_WRITE_ABSENT_ACCESS;
+        k_page_dir[i].ignored = 0x0u;
+        k_page_dir[i].page_table_addr = PDE_ABSENT_ADDR;
+    }
+    // initialize direct mapping area
+    uintptr_t page_addr = 0x0u;
+    for (; i < K_PDE_DIRECT_MAP_NUM + U_PDE_NUM; ++i) {
+        k_page_dir[i].access = K_PDE_WRITE_PRESENT_ACCESS;
+        k_page_dir[i].ignored = 0x0u;
+        k_page_dir[i].page_table_addr = pt_addr(pt_addr);
 
-  // let '%cr3' point to kernel's page dir
-  asm("mov %0, %%eax" ::"r"(cr3.page_dir_addr << 12u));
-  asm("mov %eax, %cr3");
-  // set CR0.PG = 1 and CR4.PAE = 0 to open 32-bit paging
-  asm("mov %cr4, %eax");
-  asm("or $0x80000010, %eax");
-  asm("mov %eax, %cr4");
-  asm("mov %cr0, %eax");
-  asm("or $0x80000000, %eax");
-  asm("mov %eax, %cr0");
-  // set '%esp' to its corresponding virtual address
-  asm("add $0xc0000000u, %esp");
-  // TODO: update all registers containing addresses to virtual addresses
+        page_table = (pte_t *) pt_addr;
+        for (uint32_t j = 0; j < PTE_MAX_NUM; ++j) {
+            page_table[j].access = K_PTE_WRITE_PRESENT_ACCESS;
+            page_table[j].ignored = 0x0u;
+            page_table[j].page_addr = page_addr(page_addr);
+            page_addr += PAGE_SIZE;
+        }
+        pt_addr += PT_MAX_SIZE;
+    }
+    // set all dynamic mapping area's page dir entries to absent state
+    for (; i < PDE_NUM; ++i) {
+        k_page_dir[i].access = K_PDE_WRITE_ABSENT_ACCESS;
+        k_page_dir[i].ignored = 0x0u;
+        k_page_dir[i].page_table_addr = PDE_ABSENT_ADDR;
+    }
+
+    // let '%cr3' point to kernel's page directory
+    asm("mov %0, %%eax"::"r"(k_cr3.page_dir_addr << 12u));
+    asm("mov %eax, %cr3");
+    // set CR0.PG = 1 and CR4.PAE = 0 to open 32-bit paging
+    asm("mov %cr4, %eax");
+    asm("or $0x80000010, %eax");
+    asm("mov %eax, %cr4");
+    asm("mov %cr0, %eax");
+    asm("or $0x80000000, %eax");
+    asm("mov %eax, %cr0");
+    // set '%esp' to its corresponding virtual address
+    asm("add $0xc0000000u, %esp");
+    // TODO: Update all registers containing addresses to virtual addresses and remove first 4 MB mapping.
 }
 
-void set_page_table_entry(page_table_entry_t *entry, uint8_t access,
+void set_page_table_entry(pte_t *entry, uint8_t access,
                           uint32_t address) {
-  entry->access = access;
-  entry->ignored = 0x0u;
-  entry->page_addr = address;
+    entry->access = access;
+    entry->ignored = 0x0u;
+    entry->page_addr = address;
 }
 
-void u_page_dir_init(page_dir_entry_t *page_dir) {
-  uint32_t i = 0;
-  // set user space
-  for (; i < U_PDE_NUM; ++i) {
-    page_dir[i].ignored = 0x0u;
-    page_dir[i].access = U_PDE_WRITE_ABSENT_ACCESS;
-    page_dir[i].page_table_addr = PTE_ABSENT_ADDR;
-  }
-  // copy kernel space's page dir entry
-  for (; i < PDE_NUM; ++i) {
-    page_dir_entry_t *k_page_dir = (page_dir_entry_t *)K_PAGE_DIR_PHYSICAL_ADDR;
-    page_dir[i].ignored = k_page_dir[i].ignored;
-    page_dir[i].access = k_page_dir[i].access;
-    page_dir[i].page_table_addr = k_page_dir[i].page_table_addr;
-  }
-}
-
-void u_release_pages(page_table_entry_t *page_table) {
-  for (uint32_t i = 0; i < PTE_MAX_NUM; ++i) {
-    if (page_table[i].page_addr != PTE_ABSENT_ADDR) {
-      release_page(page_no(page_table[i].page_addr));
+void u_page_dir_init(pde_t *page_dir) {
+    uint32_t i = 0;
+    // set user space
+    for (; i < U_PDE_NUM; ++i) {
+        page_dir[i].ignored = 0x0u;
+        page_dir[i].access = U_PDE_WRITE_ABSENT_ACCESS;
+        page_dir[i].page_table_addr = PTE_ABSENT_ADDR;
     }
-  }
+    // copy kernel space's page dir entry
+    for (; i < PDE_NUM; ++i) {
+        pde_t *k_page_dir = (pde_t *) K_PD_ADDR;
+        page_dir[i].ignored = k_page_dir[i].ignored;
+        page_dir[i].access = k_page_dir[i].access;
+        page_dir[i].page_table_addr = k_page_dir[i].page_table_addr;
+    }
+}
+
+void u_release_pages(pte_t *page_table) {
+    for (uint32_t i = 0; i < PTE_MAX_NUM; ++i) {
+        if (page_table[i].page_addr != PTE_ABSENT_ADDR) {
+            release_page(page_no(page_table[i].page_addr));
+        }
+    }
+}
+
+void pf_handler(intr_reg_t *intr_reg) {
+    // '%CR2' contains linear address that generated the exception.
+    uint32_t addr = get_cr(2);
+    // '%CR3' contains the physical address of page directory.
+    uint32_t cr3 = direct_mapping_virtual_addr(get_cr(3));
+    uint32_t err_code = intr_reg->err_code;
+
+    // reference an absent page
+    if (is_bit_set(err_code, 0) == FALSE) {
+        pde_t *pd = (pde_t *) cr3;
+        uint16_t pd_i = addr / PT_MAX_SIZE;
+        if (pd[pd_i].page_table_addr == PDE_ABSENT_ADDR) {
+            // page directory entry in absent state
+            pd[pd_i].page_table_addr = direct_mapping_phys_addr(request_bytes(sizeof(pte_t)));
+            pd[pd_i].ignored = 0x0u;
+            if (is_bit_set(err_code, 1) == TRUE) {
+                pd[pd_i].access = U_PDE_WRITE_PRESENT_ACCESS;
+            } else {
+                pd[pd_i].access = U_PDE_READ_PRESENT_ACCESS;
+            }
+        }
+        // set page table for absent page
+        pte_t *pt = (pte_t *) pd[pd_i].page_table_addr;
+        uint16_t pt_i = (addr << 10u >> 10u) / PAGE_SIZE;
+        pt[pt_i].ignored = 0x0u;
+        pt[pt_i].page_addr = page_phys_addr(pd_i * PTE_MAX_NUM + pt_i);
+        if (is_bit_set(err_code, 1) == TRUE) {
+            pd[pd_i].access = U_PTE_WRITE_PRESENT_ACCESS;
+        } else {
+            pd[pd_i].access = U_PTE_READ_PRESENT_ACCESS;
+        }
+        return;
+    }
+    // user-mode access
+    if (is_bit_set(err_code, 2) == TRUE) {
+        // It's a severe invalid access and we cannot fix it, so we close the process enforcedly.
+        close_proc((pcb_t *) (U_STACK_ADDR + sizeof(pcb_t)));
+        return;
+    }
+    // TODO: If more possible sources are added, we cannot determine whether it's a W/R fault like this.
+    // try to write to a read-only page
+    if (is_bit_set(err_code, 1) == TRUE) {
+        close_proc((pcb_t *) (U_STACK_ADDR + sizeof(pcb_t)));
+        return;
+    }
 }
--- kernel/memory/paging.h
+++ kernel/memory/paging.h
@@ -10,40 +10,50 @@
 #define XYOS_PAGING_H
 
 #include <stdint.h>
+#include "../interrupt/idt.h"
 
 // kernel will use 3GB through 4GB linear address space
 #define K_VIRTUAL_ADDR 0xc0000000u
 #define K_VIRTUAL_SIZE 0x80000000u
 
 // page directory's properties
-#define K_PAGE_DIR_PHYSICAL_ADDR 0x1000u
+#define K_PD_ADDR 0xc0001000u
 #define PDE_NUM 1024u
 #define U_PDE_NUM 768u
 #define K_PDE_NUM 256u
 #define K_PDE_DIRECT_MAP_NUM 16u
 #define PDE_SIZE 4u
-#define PAGE_DIR_SIZE PDE_NUM *PDE_SIZE
+#define PD_SIZE PDE_NUM *PDE_SIZE
 #define PDE_ABSENT_ADDR 0x0u
 
 /**
- * @brief PDE only reserve 20 bits to store page table's physical address,
- * so page table will be 4 KB aligned and PDE only store the upper 20 bits
+ * @def pd_addr(phys_addr)
+ *
+ * CR3 only reserve 20 bits to store page directory's physical address,
+ * so page directory will be 4 KB aligned and CR3 only store the upper 20 bits.
  */
-#define pde_addr(phys_addr) ((phys_addr) >> 12u)
+#define pd_addr(phys_addr) ((phys_addr) >> 12u)
+/**
+ * @def pt_addr(phys_addr)
+ *
+ * Only reserve 20 bits to store physical table's physical address,
+ * so page table will be 4 KB aligned and PDE only store the upper 20 bits.
+ */
+#define pt_addr(phys_addr) ((phys_addr) >> 12u)
+/**
+ * @def page_addr(phys_addr)
+ *
+ * PDE only store the upper 20 bits of the physical page's physical address.
+ */
+#define page_addr(phys_addr)((phys_addr) >> 12u)
 
 // page table's global properties
 // page tables max size in bytes
-#define PAGE_TABLE_MAX_SIZE PTE_MAX_NUM *PTE_SIZE
-#define K_PAGE_TABLE_ADDR 0xc000u
+#define PT_MAX_SIZE PTE_MAX_NUM *PTE_SIZE
+#define K_PT_ADDR 0xc000c000u
 #define PTE_MAX_NUM 1024u
 #define PTE_SIZE 4u
 #define PTE_ABSENT_ADDR 0x0u
-
-/**
- * @brief PTE only reserve 20 bits to store physical page's physical address,
- * so page table will be 4 KB aligned and PDE only store the upper 20 bits
- */
-#define pte_addr(phys_addr) ((phys_addr) >> 12u)
 
 // PDE and PTE access options, cache policy is disabled
 // PDE
@@ -65,47 +75,72 @@
 #define U_PTE_READ_ABSENT_ACCESS 0x4u
 #define U_PTE_WRITE_ABSENT_ACCESS 0x6u
 
-typedef struct page_directory_entry {
-  uint8_t access;
-  uint8_t ignored : 4;
-  // store high 20 bits because page tables are 4 KB aligned
-  uint32_t page_table_addr : 20;
-} __attribute__((packed)) page_dir_entry_t;
-
-typedef struct page_table_entry {
-  uint16_t access : 9;
-  uint8_t ignored : 3;
-  uint32_t page_addr : 20;
-} __attribute__((packed)) page_table_entry_t;
-typedef struct CR3 {
-  // 2:0 in CR3 is ignored
-  uint8_t access : 5;
-  uint8_t ignored : 7;
-  uint32_t page_dir_addr : 20;
-} CR3_t;
-/*typedef uint32_t pde_t;
-typedef uint32_t pte_t;*/
+/**
+ * @typedef pde_t page directory entry
+ *
+ * Page directory must be 4 KB aligned.
+ */
+typedef struct pde {
+    uint8_t access;
+    uint8_t ignored: 4;
+    // store high 20 bits because page tables are 4 KB aligned
+    uint32_t page_table_addr: 20;
+} __attribute__((packed)) pde_t;
 
 /**
- * @brief Initialize kernel's page directory. Initialize 64 MB direct mapping
+ * @typedef pte_t page table entry
+ *
+ * Page table must be 4 KB aligned.
+ */
+typedef struct pte {
+    uint16_t access: 9;
+    uint8_t ignored: 3;
+    uint32_t page_addr: 20;
+} __attribute__((packed)) pte_t;
+
+typedef struct CR3 {
+    // 2:0 in CR3 is ignored
+    uint8_t access: 5;
+    uint8_t ignored: 7;
+    uint32_t page_dir_addr: 20;
+} CR3_t;
+
+/**
+ * Initialize kernel's page directory. Initialize 64 MB direct mapping
  * area to the physical address space from 0x0. Set remaining 960 MB kernel
  * space reserved for dynamic mapping.
  */
 void k_paging_init();
 
 /**
- * @brief Creating a new process will use this function to set its page
- * directory. It will set all user space entry to absent state and copy kernel
+ * Creating a new process will use this function to set its page directory.
+ * It will set all user space entry to absent state and copy kernel
  * space entry in corresponding location. Caller should request memory for page
  * directory itself.
- * @param page_directory
+ *
+ * @param page_dir
  */
-void u_page_dir_init(page_dir_entry_t *page_directory);
+void u_page_dir_init(pde_t *page_dir);
 
 /**
- * @brief Release all pages stored in 'page_table'.
+ * Release all pages exist in 'page_table'. Notice that 'page_table' itself should be released by caller itself.
+ *
  * @param page_table
  */
-void u_release_pages(page_table_entry_t *page_table);
+void u_release_pages(pte_t *page_table);
+
+/**
+ * @brief page fault handler
+ *
+ * @verbatim
+ * Now there are only 3 possible sources of PF exception.
+ * 1. page not present
+ * 2. invalid mode:
+ *      - kernel access address below than 0xc0000000u
+ *      - user access address above than 0xc0000000u
+ * 3. write to a read-only page
+ * @endverbatim
+ */
+void pf_handler(intr_reg_t *intr_reg);
 
 #endif // XYOS_PAGING_H
